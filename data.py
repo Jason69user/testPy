5cde3a094975cbfcefbca152142125ecae176280
Dimitry <dartahilles@mail.ru>
2025-04-09 16:03:40 +0400

    Переннес в функции и добавил исключения

--- data.py
+++ data.py
@@ -1,20 +1,54 @@
-with open('data.txt', 'w') as file:
-    file.write("Chocolate's embrace\n"),
-    file.write("Soft caramel dream at hand,\n")
-    file.write("Snickers in my grip.")
+def write_file():
+    try:
+        with open('data.txt', 'w') as file:
+            file.write("Chocolate's embrace\n")
+            file.write("Soft caramel dream at hand,\n")
+            file.write("Snickers in my grip.")
+    except IOError:
+        print('Ошибка ввода-вывода')
 
-with open('data.txt', 'r') as file:
-    content = file.read()
-    print(content)
+def read_file():
+    try:
+        with open('data.txt', 'r') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print('Файл не найден')
+    except PermissionError:
+        print("Нет прав доступа к файлу.")
 
-with open('data.txt', 'a')as file:
-    file.writelines('\nIt was delicious for everyone')
-    file.close()
+def append_file():
+    try:
+        with open('data.txt', 'a') as file:
+            file.write('\nIt was delicious for everyone')
+    except FileNotFoundError:
+        print('Файл не найден')
+    except PermissionError:
+        print("Нет прав доступа к файлу.")
 
-with open('data.txt', 'r') as file:
-    for line in file:
-        print(line, end='')
+def read_line():
+    try:
+        with open('data.txt', 'r') as file:
+            for line in file:
+                print(line)
+    except FileNotFoundError:
+        print('Файл не найден')
+    except PermissionError:
+        print("Нет прав доступа к файлу.")
 
-with open('data.txt', 'rb') as source_file:
-    with open('data_copy.txt', 'wb') as copy_file:
-        copy_file.write(source_file.read())
+def copy_file():
+    try:
+        with open('data.txt', 'rb') as source_file:
+            with open('data_copy.txt', 'wb') as copy_file:
+                copy_file.write(source_file.read())
+    except FileNotFoundError:
+        print('Файл не найден')
+    except PermissionError:
+        print("Нет прав доступа к файлу.")
+
+
+write_file()
+read_file()
+append_file()
+read_line()
+copy_file()
